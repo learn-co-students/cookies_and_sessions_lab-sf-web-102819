77b74e189afdffc84f26a46cfb2995f101a60aeb
github username <github email address>
2019-12-02 12:00:34 -0800

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+
+    def index
+    end
+  
+    def add
+        cart << params[:product]
+        render :index
+      end
+  
+  end
+  
